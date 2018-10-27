a6c2fe9d29864d91b24e92cf4dd8496677cc888e
Ana Lu <analucia.spinola@usp.br>
2018-10-26 21:37:29 -0300

    Initial commit

---
+++ docs/script.js
@@ -0,0 +1,29 @@
+function Enviar() {
+ 
+    var mulher = document.getElementById("mulherid");
+ 
+    if (mulher.value === "sim") {
+           alert('Ok, vá para a questão dois')}
+    else if (mulher.value ==="não") {
+        alert('Obrigada pela participação, mas você foi eliminado')}
+    }
+
+function Enviar2() {
+ 
+    var feminista = document.getElementById("feministaid")
+ 
+    if (feminista.value === "sim") {
+           alert('Ok, vá para a questão três')}
+    else if (feminista.value ==="não") {
+        alert('Obrigada pela participação, mas você foi eliminada')}
+}
+
+function Enviar3(){
+ 
+    var bozo = document.getElementById("bozoid");
+ 
+    if (bozo.value === "sim") {
+           alert('Procure um psquiatra')}
+    else if (mulher.value ==="não") {
+        alert('Obrigada pela participação, você é muito esperta!')}
+    }
