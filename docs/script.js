1ec6d583fb4315000a4c312b7b48491c05e26f6c
Ana Lucia Spinola <37885167+analuspi@users.noreply.github.com>
2018-10-27 09:36:55 -0300

    mudei var por let e adicionei .value

--- docs/script.js
+++ docs/script.js
@@ -1,37 +1,37 @@
 function Enviar() {
  
-    var mulher = document.getElementById("mulherid").toLowerCase();
- return false
+    let mulher = document.getElementById("mulherid").value.toLowerCase();
+ 
     if (mulher.value === "SIM") {
            alert('Ok, vá para a questão dois')}
     else  {
         alert('Obrigada pela participação, mas você foi eliminado')
     }
- 
+ return false
 }
 
 function Enviar2() {
  
-    var feminista = document.getElementById("feministaid"). toLowerCase();
- return false
+    let feminista = document.getElementById("feministaid").value.toLowerCase();
+ 
     if (feminista.value === "SIM") {
            alert('Ok, vá para a questão três');
     }
     else  {
         alert('Obrigada pela participação, mas você foi eliminada');
   }
- 
+ return false
 }
 
 function Enviar3(){
  
-    var bozo = document.getElementById("bozoid").toLowerCase;
- return false
+    let bozo = document.getElementById("bozoid").value.toLowerCase;
+ 
     if (bozo.value === "SIM") {
            alert('Procure um psquiatra');
     }
     else {
         alert('Obrigada pela participação, você é muito esperta!');
     }
- 
+ return false
  }
